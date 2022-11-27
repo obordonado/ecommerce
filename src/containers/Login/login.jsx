adf6e5d588182956834ea3f2bb7454deb903f4ed
obordonado <obordonado@protonmail.com>
2022-11-27 19:11:45 +0100

    fix: form height

--- src/containers/Login/login.jsx
+++ src/containers/Login/login.jsx
@@ -62,17 +62,89 @@ const logIn = () => {
 };
     
   return (
-    
-  <div className='login'>
 
-    <div className='loginDesign'>
-      <input  type='email' name='email' placeholder='email' onChange={updateCredentials} lenght='30'/>
-      <input  type='password'  name='password' placeholder='password' onChange={updateCredentials} lenght='30'/>
-      <div className="button" onClick={()=>logIn()}>Login</div>
-      <div>{msgError}</div>
-    </div>
-</div>
-  )
+    <div className="register">
+    <>
+      <div className="contenedor">
+        <div className="form">
+          <form onSubmit={handleRegister}>
+            <h2 className="register">Register</h2>
+            <input
+              required
+              type="text"
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <input
+              required
+              type="text"
+              placeholder="Surname"
+              value={surname}
+              onChange={(e) => setSurname(e.target.value)}
+            />
+            <input
+              required
+              type="text"
+              placeholder="Phone number"
+              value={phone_number}
+              onChange={(e) => setPhone_number(e.target.value)}
+            />
+            <input
+              required
+              type="text"
+              placeholder="Bizum / Paypal"
+              value={payment_type}
+              onChange={(e) => setPayment_type(e.target.value)}
+            />
+            <input
+              required
+              type="text"
+              placeholder="Address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+            <input
+              required
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+              required
+              type="password"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button className="submit" type="submit" value="Enviar">
+              Send
+            </button>
+          </form>
+          <div className="error">{msgError}</div>
+        </div>
+      </div>
+    </>
+  </div>
+);
+
+
+
+
+
+
+    
+//   <div className='login'>
+
+//     <div className='loginDesign'>
+//       <input  type='email' name='email' placeholder='email' onChange={updateCredentials} lenght='30'/>
+//       <input  type='password'  name='password' placeholder='password' onChange={updateCredentials} lenght='30'/>
+//       <div className="button" onClick={()=>logIn()}>Login</div>
+//       <div>{msgError}</div>
+//     </div>
+// </div>
+//   )
 }
 
 export default login
