3db058f2582b572401e199de4dc36752af012d68
obordonado <obordonado@protonmail.com>
2022-11-30 12:09:13 +0100

    fix: new url due to new service on railway DB

--- src/containers/Kids/functions.js
+++ src/containers/Kids/functions.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const allProducts= async (state)=>{
-    const peticion = await axios.get('https://devos-ecommerce.herokuapp.com/api/getallproducts')
+    const peticion = await axios.get('https://devos-ecommerce-laravel-backend-production.up.railway.app/api/getallproducts')
     // console.log(peticion.data.data);
     ///devuelve un array
     state(peticion.data.data)/// Lo mando a state para usarlo en Inicio.jsx
 };
 
 const productById = async (id, state)=>{
-    const peticion = await axios.get(`https://devos-ecommerce.herokuapp.com/api/getproductbyid/${id}`)
+    const peticion = await axios.get(`https://devos-ecommerce-laravel-backend-production.up.railway.app/api/api/getproductbyid/${id}`)
     // console.log(peticion.data);
     ///devuelve un objeto
     state(peticion.data.data) /// Lo mando al Personaje.jsx (justo debajo de la funcion const Personaje()
